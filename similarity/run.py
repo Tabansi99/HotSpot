b285b8ef75531168e7b0bb8d42f170954c130f25
Jason Raether <jasonraether@tamu.edu>
2022-03-26 23:35:26 -0500

    jaccard and cosine distance

--- similarity/run.py
+++ similarity/run.py
@@ -4,6 +4,7 @@ from collections import Counter, defaultdict
 from text_utils import preprocess_text
 import pandas as pd
 import numpy as np
+from scipy import spatial
 
 ### Constants ###
 MAX_WORDS = 50
@@ -39,17 +40,38 @@ base_df = pd.DataFrame(
 j_df = base_df.copy(deep=True)
 
 for c1 in courses:
+	c1_set = set([w for w in preprocess_text(c1.desc) if w in top_set])
 	for c2 in courses:
 		if c1 != c2:
-			c1_set = set([w for w in preprocess_text(c1.desc) if w in top_set])
 			c2_set = set([w for w in preprocess_text(c2.desc) if w in top_set])
 
 			j_df[str(c1)][str(c2)] = len(c1_set.intersection(c2_set))/len(c1_set.union(c2_set))
 			
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-	print('Most similar based on Jaccard:')
+	print('Most similar based on Jaccard distance:')
 	print(j_df.idxmax(axis=1))
 
+print('\n===============================\n')
 ### Cosine similarity ###
 c_df = base_df.copy(deep=True)
-#########################
+
+for c1 in courses:
+	c1_vec = np.zeros(MAX_WORDS)
+	c1_words = [w for w in preprocess_text(c1.desc) if w in top_set]
+	for w in c1_words:
+		c1_vec[w2i[w]] = 1
+
+	for c2 in courses:
+		if c1 != c2:
+			c2_vec = np.zeros(MAX_WORDS)
+			c2_words = [w for w in preprocess_text(c2.desc) if w in top_set]
+			for w in c2_words:
+				c2_vec[w2i[w]] = 1
+
+			c_df[str(c1)][str(c2)] = 1 - spatial.distance.cosine(c1_vec, c2_vec)
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+	print('Most similar based on Cosine similarity:')
+	print(c_df.idxmax(axis=1))
+
+print('\n===============================\n')
