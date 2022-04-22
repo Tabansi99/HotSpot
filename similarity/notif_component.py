ca64293eacbd4f51f7ff39ff8dbbcd40e3bcf1b1
Rohit Vaidyula <vaidyular@gmail.com>
2022-04-22 17:50:57 -0500

    Notif System module added. Just have to pass the reciever email and the name of the course

---
+++ similarity/notif_component.py
@@ -0,0 +1,12 @@
+import yagmail
+yag = yagmail.SMTP('jd650293@gmail.com', 'HotSpot123')
+
+def send_signup_email(reciever_email, course_number):
+    content = ['Congratulations, your are successfully signed up for ' + course_number]
+    yag.send(reciever_email, 'Course Sign-Up Success', content)
+
+def notify_me_email(reciever_email, course_number):
+    content = ['You will be notified when ' + course_number + ' becomes available']
+    yag.send(reciever_email, 'Course Availability Notification', content)
+
+
